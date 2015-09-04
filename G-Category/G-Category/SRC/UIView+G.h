ce600cb9e149bd674a7921909f38092061d8e4f5
鲁志刚 <se1ing@hotmail.com>
2015-09-04 15:41:11 +0800

    finishi

---
+++ G-Category/G-Category/SRC/UIView+G.h
@@ -0,0 +1,13 @@
+//
+//  UIView+G.h
+//  G-Category
+//
+//  Created by Alfie on 15/9/4.
+//  Copyright (c) 2015年 Bisbana.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (G)
+
+@end
---
+++ G-Category/G-Category/SRC/UIView+G.m
@@ -0,0 +1,21 @@
+//
+//  UIView+G.m
+//  G-Category
+//
+//  Created by Alfie on 15/9/4.
+//  Copyright (c) 2015年 Bisbana.com. All rights reserved.
+//
+
+#import "UIView+G.h"
+
+@implementation UIView (G)
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
